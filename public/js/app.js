e1781deced075881a5d3f55170fb3461654e63ab
Daniel Wadell <danielwadell@daniels-mbp.home>
2019-08-14 16:39:58 -0700

    added fetch to client side javascript

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Client side javascript is here!')
+
+fetch('http://localhost:3000/weather?address=yucaipa').then((response) => {
+    response.json().then(({location, forecast, error} = {}) => {
+        if (error) {
+            return console.log('Wrong location entered!')
+        }
+        console.log(location)
+        console.log(forecast)
+    })
+})
