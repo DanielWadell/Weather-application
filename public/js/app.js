838071916dba88318aff15d18d47f3bbb5749aee
Daniel Wadell <danielwadell@daniels-mbp.home>
2019-08-14 17:07:09 -0700

    created search form and event listener

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,22 @@
 console.log('Client side javascript is here!')
 
-fetch('http://localhost:3000/weather?address=yucaipa').then((response) => {
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then(({location, forecast, error} = {}) => {
         if (error) {
-            return console.log('Wrong location entered!')
+            return console.log(error)
         }
-        console.log(location)
         console.log(forecast)
+        console.log(location)
     })
+})
 })
