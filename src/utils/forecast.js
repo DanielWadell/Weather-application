e701d7948b237efa9783c1f7e42fbcc96e435491
Daniel Wadell <danielwadell@daniels-mbp.home>
2019-08-14 18:03:59 -0700

    weather appears after search

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Incorrect location has been entered!', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            const rainProbability = body.currently.precipProbability*100
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees. There is a ' + rainProbability + '% chance of rain.')
         }
     })
 }
